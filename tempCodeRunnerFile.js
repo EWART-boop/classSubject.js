083e16c7811b8fcdbd822de1218c029d41e7b641
EWART-boop <johnewartdelacruz@gmail.com>
2024-11-04 18:11:54 +0800

    classSubject.js.

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+let subjectTitle = "Introduction to JavaScript"; 
+let classSchedule = "Monday and Wednesday, 10:00 AM - 11:30 AM"; 
+let classroom = "Room 204"; 
+let classInstructor = "Dr. Alexandro"; 
+let studentName = "Robinson De Guzman"; 
+
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
